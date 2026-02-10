54f0fc894123624f322b4d195630940c691c487c
Louise <emmalouisekarlsson@hotmail.com>
2026-02-10 08:05:41 +0100

    EntityFramework

---
+++ BookApp/Models/BookDb.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApp.Models;
+
+public class BookDb(DbContextOptions<BookDb> options) : DbContext(options)
+{
+    public DbSet<Book> Books => Set<Book>();
+}
