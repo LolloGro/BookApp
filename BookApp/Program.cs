317c85ea911e64c77a1fd35a2497cad57aea00ba
Louise <emmalouisekarlsson@hotmail.com>
2026-02-10 08:06:27 +0100

    Added connection to database and creating of tables in database

--- BookApp/Program.cs
+++ BookApp/Program.cs
@@ -1,3 +1,6 @@
+using BookApp.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Connection to database 
+builder.Services.AddDbContext<BookDb>(options => 
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 45)),
+        mySqlOptions => mySqlOptions.EnableRetryOnFailure()));
 
 var app = builder.Build();
 
@@ -17,6 +26,29 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+//Replaces 'dotnet ef database update' and automatically creates tables in database when entering 'docker compose up --build'   
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BookDb>();
+    
+    var retries = 10;
+
+    for (int i = 0; i < retries; i++)
+    {
+        try
+        {
+            db.Database.Migrate();
+            Console.WriteLine("Database migrated");
+            break;
+        }
+        catch (MySqlConnector.MySqlException)
+        {
+            Console.WriteLine("Database migration failed, retrying... in 5 seconds");
+            Thread.Sleep(5000);
+        }
+    }
+}
 
 app.MapControllers();
 
