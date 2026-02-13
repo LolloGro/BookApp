58d061f86a51f0f786a2116025f3adc4e2478397
Louise <emmalouisekarlsson@hotmail.com>
2026-02-13 13:59:29 +0100

    Added header for reuse in app

---
+++ Frontend/BookFrontend/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {AppHeader} from './components/app-header/app-header';
+
+@Component({
+  selector: 'app-root',
+  imports: [AppHeader, RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('BookFrontend');
+}
---
+++ Frontend/BookFrontend/src/app/components/app-header/app-header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-app-header',
+  imports: [RouterModule],
+  templateUrl: './app-header.html',
+  styleUrl: './app-header.css',
+})
+export class AppHeader {
+
+}
