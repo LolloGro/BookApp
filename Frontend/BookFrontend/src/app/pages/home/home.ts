554c4b8188e7ecc0c96df548c60b01e418c57bbe
Louise <emmalouisekarlsson@hotmail.com>
2026-02-17 16:23:30 +0100

    Adjusted homepage by adding image and adding check if user is logged in to alter which text is displayed

--- Frontend/BookFrontend/src/app/pages/home/home.ts
+++ Frontend/BookFrontend/src/app/pages/home/home.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {AsyncPipe} from '@angular/common';
+import {LoginService} from '../../services/login.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    RouterLink,
+    AsyncPipe
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-
+private loginService = inject(LoginService);
+isLoggedIn$ = this.loginService.loggedIn$
 }
