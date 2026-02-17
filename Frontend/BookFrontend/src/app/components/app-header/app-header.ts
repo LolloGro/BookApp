6c100d267b56eb2324c0331f9a43a3c75f6e1c89
Louise <emmalouisekarlsson@hotmail.com>
2026-02-17 16:21:49 +0100

    Added automic closing of navbar after clicking link

--- Frontend/BookFrontend/src/app/components/app-header/app-header.ts
+++ Frontend/BookFrontend/src/app/components/app-header/app-header.ts
@@ -1,21 +1,38 @@
-import { Component, inject} from '@angular/core';
+import { Component, inject, AfterViewInit} from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import {LoginService} from '../../services/login.service';
 import {AsyncPipe} from '@angular/common';
 import {ThemeService} from '../../services/theme.service';
 
+declare var bootstrap: any;
 @Component({
   selector: 'app-app-header',
   imports: [RouterModule, AsyncPipe],
   templateUrl: './app-header.html',
   styleUrl: './app-header.css',
 })
-export class AppHeader{
+export class AppHeader implements AfterViewInit {
+  private onClose: any;
+
   private loginService = inject(LoginService);
   public themeService = inject(ThemeService);
   private router = inject(Router);
 
   isLoggedIn$ = this.loginService.loggedIn$;
+
+  ngAfterViewInit() {
+    const close = document.getElementById('navbarSupportedContent');
+    if(close){
+      this.onClose = new bootstrap.Collapse(close, {toggle: false});
+    }
+  }
+
+  closeNavbar(){
+    if(this.onClose){
+      this.onClose.hide();
+    }
+  }
+
 
   onLogout(){
     this.loginService.logout();
